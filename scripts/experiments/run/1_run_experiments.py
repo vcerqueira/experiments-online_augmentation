4d75be18232c57bc2eb3bd13ff6e291d5ba873af
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2024-11-08 16:17:56 +0000

    pipeline with apriori aug

--- scripts/experiments/run/1_run_experiments.py
+++ scripts/experiments/run/1_run_experiments.py
@@ -1,7 +1,9 @@
+from functools import partial
+import pandas as pd
 from neuralforecast import NeuralForecast
 from neuralforecast.models import NHITS
-from neuralforecast.losses.numpy import smape
-from datasetsforecast.evaluation import accuracy
+from utilsforecast.losses import mase
+from utilsforecast.evaluation import evaluate
 
 from metaforecast.utils.data import DataUtils
 from metaforecast.synth.callbacks import OnlineDataAugmentationCallback
@@ -18,37 +20,53 @@ df, horizon, n_lags, freq_str, freq_int = data_loader.load_everything(group, min
 print(df['unique_id'].value_counts())
 print(df.shape)
 
+MODEL_CONFIG = {
+    'input_size': n_lags,
+    'h': horizon,
+    'start_padding_enabled': True,
+    'accelerator': 'mps',
+    'max_steps': 2000,
+    'val_check_steps': 30,
+    'enable_checkpointing': True,
+    'early_stop_patience_steps': 30,
+}
+
+# setup
+
 train, test = DataUtils.train_test_split(df, horizon)
 
 tsgen = SeasonalMBB(seas_period=freq_int)
-
 augmentation_cb = OnlineDataAugmentationCallback(generator=tsgen)
-# create apriori pipeline
-# https://lightning.ai/docs/pytorch/stable/extensions/callbacks.html
 
+models = [NHITS(**MODEL_CONFIG), NHITS(**MODEL_CONFIG, callbacks=[augmentation_cb])]
+models_da = [NHITS(**MODEL_CONFIG)]
 
-models = [NHITS(input_size=horizon,
-                h=horizon,
-                start_padding_enabled=True,
-                accelerator='mps'),
-          NHITS(input_size=horizon,
-                h=horizon,
-                start_padding_enabled=True,
-                accelerator='mps',
-                callbacks=[augmentation_cb])]
+# using original train
 
 nf = NeuralForecast(models=models, freq=freq_str)
-
-nf.fit(df=train)
+nf.fit(df=train, val_size=horizon)
 
 fcst = nf.predict()
 fcst = fcst.rename(columns={'NHITS1': 'NHITS(MBB)'})
 
-fcst.head()
+# using augmented train
+apriori_tsgen = SeasonalMBB(seas_period=freq_int)
+
+train_synth = apriori_tsgen.transform(train)
+
+train_ext = pd.concat([train, train_synth]).reset_index(drop=True)
+
+nf2 = NeuralForecast(models=models_da, freq=freq_str)
+nf2.fit(df=train_ext, val_size=horizon)
+
+fcst_ext = nf2.predict()
+fcst_ext = fcst_ext.rename(columns={'NHITS': 'NHITS(ApMBB)'})
+
+# test set and evaluate
 
 test = test.merge(fcst.reset_index(), on=['unique_id', 'ds'], how="left")
-
-evaluation_df = accuracy(test, [smape], agg_by=['unique_id'])
+test = test.merge(fcst_ext.reset_index(), on=['unique_id', 'ds'], how="left")
+evaluation_df = evaluate(test, [partial(mase, seasonality=52)], train_df=train)
 
 eval_df = evaluation_df.drop(columns=['metric', 'unique_id'])
 
